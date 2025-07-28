7c89ada1a56d4a855ec1cb4b451476044b6a8173
Mercocamp <administrativo@mercocamp.com>
2025-07-27 23:07:34 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <-- ESTA É A LINHA NOVA E IMPORTANTE
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
